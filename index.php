286f588f90a2809e38e1bf996610f67b641ef6f4
SkillBayz <139914734+SkillBayz@users.noreply.github.com>
2023-09-23 19:10:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>DrinkKing</title>
+
+<link href="dk-mall.css" rel="stylesheet" type="text/css">
+<link rel="icon" href="icon.ico">
+<link rel="stylesheet" href="https://use.typekit.net/zbb3hig.css">
+</head>
+
+<body>
+	
+	<div id="container">
+		
+		<header>
+		<div id="left-nav">
+		<a href="index.php" id="logo"><img src="drinkkinglogo2.png"></a>
+		<a href="#">Produkter</a>
+		<a href="#">Motiv</a>
+		<a href="#">Kontakt</a>
+		<a href="#">Frågor &amp; Svar</a>
+		</div>
+			
+		<div id="mid-nav">
+			<iconify-icon icon="iconamoon:search-light" height="18px"></iconify-icon>
+			<input type="search" id="search" placeHolder="Sök bland våra produkter..." autocomplete="off">
+		</div>
+		
+		<div id="right-nav">
+		<a href="#" id="favoriter"><iconify-icon icon="ph:heart" height="28px"></iconify-icon></a>
+		<a href="#" id="profil"><iconify-icon icon="octicon:person-24" height="28px"></iconify-icon></iconify-icon></a>
+		<a href="#" id="kundvagn"><iconify-icon icon="solar:cart-linear" height="28px"></iconify-icon></a>
+		</div>
+		</header>
+		
+		
+		<main>
+		<div id="poster">
+			<h2><span style="color:#208AAE">Drink</span> Like a King</h2>
+			<p>Vi skapar unika och personliga glas med egentryckta motiv för minnesvärda tillfällen. Våra exklusiva glas ger en personlig touch till hemmabaren och kan bidra som en vacker inredningsdetalj i hemmet.</p>
+			<a href="#" class="handla"><iconify-icon icon="solar:cart-linear" height="28px"></iconify-icon>&nbsp;Handla nu </a>
+		</div>
+		<center><div id="glastyper">
+			<div class="pil">
+				<button onClick="scrollL()"><iconify-icon icon="ep:arrow-up-bold" height="25px" rotate="270deg"></button>
+			</div>
+			<div class="cover">
+				<div class="scroll-glas">
+				<a href="#"><div class="glastyp"><img src="olglas.png"><p>Ölglas</p></div></a>
+				<a href="#"><div class="glastyp"><img src="vinglas.png"><p>Vinglas</p></div></a>
+				<a href="#"><div class="glastyp"><img src="olglas.png"><p>Drinkglas</p></div></a>
+				<a href="#"><div class="glastyp"><img src="shotglas.png"><p>Shotglas</p></div></a>
+				<a href="#"><div class="glastyp"><img src="olglas.png"><p>Dricksglas</p></div></a>
+				<a href="#"><div class="glastyp"><img src="olglas.png"><p>Champangeglas</p></div></a>
+				<a href="#"><div class="glastyp"><img src="olglas.png"><p>Whiskyglas</p></div></a>
+				<a href="#"><div class="glastyp"><img src="olglas.png"><p>Fickplunta</p></div></a>
+				<a href="#"><div class="glastyp"><img src="olglas.png"><p>Tillbehör</p></div></a>
+				</div>
+			</div>
+		    <div class="pil">
+				<button onClick="scrollR()"><iconify-icon icon="ep:arrow-up-bold"  height="25px" rotate="90deg"></iconify-icon></button>
+			</div>
+		</div></center>
+		<h2>Toppsäljare</h2>
+		<div id="toppsell">
+				
+				    <div class="produkt">
+			        <span class="add-fav"><iconify-icon icon="ph:heart" height="28px"></iconify-icon></span>
+			        <a href="#"><div class="info"><img src="glas1.png">
+			        <center><h4>Etched Landskrona BOIS Ölglas 50cl</h4></center></div></a>
+			        <p>89kr</p>
+			        <center><span class="add-vagn"><iconify-icon icon="solar:cart-linear" height="28px">
+			        </iconify-icon><h4>Lägg i varukorg</h4></span></center>
+			        </div>
+					<div class="produkt">
+			        <span class="add-fav"><iconify-icon icon="ph:heart" height="28px"></iconify-icon></span>
+			        <a href="#"><div class="info"><img src="glas1.png">
+			        <center><h4>Etched Landskrona BOIS Ölglas 50cl</h4></center></div></a>
+			        <p>89kr</p>
+			        <center><span class="add-vagn"><iconify-icon icon="solar:cart-linear" height="28px">
+			        </iconify-icon><h4>Lägg i varukorg</h4></span></center>
+			        </div>
+					<div class="produkt">
+			        <span class="add-fav"><iconify-icon icon="ph:heart" height="28px"></iconify-icon></span>
+			        <a href="#"><div class="info"><img src="glas1.png">
+			        <center><h4>Etched Landskrona BOIS Ölglas 50cl</h4></center></div></a>
+			        <p>89kr</p>
+			        <center><span class="add-vagn"><iconify-icon icon="solar:cart-linear" height="28px">
+			        </iconify-icon><h4>Lägg i varukorg</h4></span></center>
+			        </div>
+					<div class="produkt">
+			        <span class="add-fav"><iconify-icon icon="ph:heart" height="28px"></iconify-icon></span>
+			        <a href="#"><div class="info"><img src="glas1.png">
+			        <center><h4>Etched Landskrona BOIS Ölglas 50cl</h4></center></div></a>
+			        <p>89kr</p>
+			        <center><span class="add-vagn"><iconify-icon icon="solar:cart-linear" height="28px">
+			        </iconify-icon><h4>Lägg i varukorg</h4></span></center>
+			        </div>
+					<div class="produkt">
+			        <span class="add-fav"><iconify-icon icon="ph:heart" height="28px"></iconify-icon></span>
+			        <a href="#"><div class="info"><img src="glas1.png">
+			        <center><h4>Etched Landskrona BOIS Ölglas 50cl</h4></center></div></a>
+			        <p>89kr</p>
+			        <center><span class="add-vagn"><iconify-icon icon="solar:cart-linear" height="28px">
+			        </iconify-icon><h4>Lägg i varukorg</h4></span></center>
+			        </div>
+		    
+		</div>
+		<center><a href="#" class="handla mer"><iconify-icon icon="icon-park-outline:more-two" height="28px"></iconify-icon>Visa fler</a></center>
+		<div class="toppmotiv">
+			
+		<a href="#">
+			<h3>Bli en äkta supporter genom att köpa ett glas med ditt lag!</h3>
+			<h4>Välj bland 20+ klubbmärken att ha på ditt glas</h4>
+			<p>Titta in våra glas med clubmärkes motiv. Finns varianter i både frostat och färgat. </p>
+		</a>
+		<a href="#">
+			<h3>Bli en äkta supporter genom att köpa ett glas med ditt lag!</h3>
+			<h4>Välj bland 20+ klubbmärken att ha på ditt glas</h4>
+			<p>Titta in våra glas med clubmärkes motiv. Finns varianter i både frostat och färgat. </p>
+		</a>
+			
+		</div>
+		<div class="features">
+			<div class="feature">
+				<iconify-icon icon="la:shipping-fast" height="34px"></iconify-icon>
+				<p>SNABB LEVERANS</p>
+			</div>
+			<div class="feature">
+				<iconify-icon icon="fluent:credit-card-clock-28-regular" height="34px"></iconify-icon>
+				<p>BETALA SENARE</p>
+			</div>
+			<div class="feature">
+				<iconify-icon icon="mdi:encryption-secure-outline" height="34px"></iconify-icon>
+				<p>100% SÄKER CHECKOUT</p>
+			</div>
+		</div>
+		<div id="om">
+			<h2>DrinkKing UF</h2>
+			<p>Vi öppnade våra dörrar i början av september 2023 med en gemensam passion och en vision om att erbjuda något unikt och minnesvärt till våra kunder. Vad var motivet bakom att starta DrinkKing? Vi tror på att göra varje ögonblick minnesvärt och varje dryck speciell. Vårt mål var att skapa ett känt märke på glas över hela Sverige och göra vårt första år så framgångsrikt som möjligt. Med tiden strävar vi efter att utvecklas till ett riktigt bolag och göra vårt sortiment ännu bredare.</p>
+			<p>Vi är inte bara ett företag som säljer glas. Vi är skapare av unika konstverk som berikar dina upplevelser. Våra produkter omfattar allt från champagneglas och whiskyglas till fickpluntor och underlägg. Vad som gör oss unika är våra tryck – konstnärliga och personliga, helt enkelt något som inte går att hitta någon annanstans. Varje glas är en berättelse, och varje gåva är ett uttryck av kärlek och omsorg.</p>
+			<p>Vi vet att våra kunder förtjänar det allra bästa. Därför är varje glas noggrant utformat och tillverkat med omsorg. Vår produktion är hållbar, och vi strävar efter att minimera vår miljöpåverkan.</p>
+			<p>Vårt team består av tre passionerade killar som går sitt tredje år på gymnasiet. Vi brinner för att skapa något speciellt för dig, något personligt som du kan vara stolt över och använda vid speciella tillfällen. Vårt uppdrag är att göra varje ögonblick unikt och minnesvärt genom våra glas.</p>
+			<p>Skål för att du väljer DrinkKing!</p>
+		</div>
+		
+		</main>
+		
+		
+		<footer>
+			<div id="fot-meny">
+				<div class="fot-lista">
+					<h3>Handla</h3>
+					<a href="#">Ölglas</a>
+					<a href="#">Vinglas</a>
+					<a href="#">Drinkglas</a>
+					<a href="#">Shotglas</a>
+					<a href="#">Dricksglas</a>
+					<a href="#">Champangeglas</a>
+					<a href="#">Whiskyglas</a>
+					<a href="#">Fickplunta</a>
+					<a href="#">Tillbehör</a>
+				</div>
+				<div class="fot-lista">
+					<h3>DrinkKing UF</h3>
+					<a href="#">Om oss</a>
+					<a href="#">Användarvilkor</a>
+					<a href="#">Kontakt</a>
+					<a href="#">Frågor &amp; Svar</a>
+					<a href="#">Leverans</a>
+					<a href="#">Betalningsalternativ</a>
+					<a href="#">Affiliate</a>
+				</div>
+				<div class="fot-lista">
+					<h3>Följ oss</h3>
+					<a href="#"><div><iconify-icon icon="ic:baseline-tiktok" height="26px"></iconify-icon><span>TikTok</span></div></a>
+					<a href="#"><div><iconify-icon icon="mdi:instagram" height="26px"></iconify-icon><span>Instagram</span></div></a>
+					
+				</div>
+			
+			</div>
+		
+
+		</footer>
+	
+	</div>
+
+</body>
+<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+<script>
+	function scrollL(){
+		var left = document.querySelector(".scroll-glas");
+		left.scrollBy(-250, 0);
+	}
+	function scrollR(){
+		var right = document.querySelector(".scroll-glas");
+		right.scrollBy(250, 0);
+	}
+
+</script>
+</html>
